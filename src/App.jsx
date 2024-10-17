e48533c8003f173fe3ee64c589b870c521d04b1e
Ahmad Ali <xevenahmad@gmail.com>
2024-10-17 12:22:09 +0500

    complete mui basics

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,40 @@ import "@fontsource/roboto/700.css";
 import {
   Typography,
   AppBar,
-  Card,
-  CardActions,
-  CardMedia,
-  CardContent,
   CssBaseline,
+  Container,
+  Button,
+  ButtonGroup,
   Grid,
   Toolbar,
-  Container,
+  Box,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
 } from "@mui/material";
 import PhotoCamera from "@mui/icons-material/PhotoCamera";
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+
 function App() {
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   return (
     <>
-      {/* <Typography variant='h1'>Hello</Typography> */}
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
           <PhotoCamera />
-          <Typography variant="h6">Hello</Typography>
+          <Typography variant="h6" sx={{ marginLeft: "20px" }}>
+            Hello
+          </Typography>
         </Toolbar>
       </AppBar>
       <main>
-        <div>
+        <Box
+          sx={{
+            padding: (theme) => theme.spacing(8, 0, 6), // leveraging theme spacing
+          }}
+        >
           <Container maxWidth="sm">
             <Typography
               variant="h2"
@@ -53,15 +62,70 @@ function App() {
               facere velit odit ipsum similique officia.
             </Typography>
           </Container>
-        </div>
-        <div>
+        </Box>
+        <div style={{display: 'flex', justifyContent: 'center', marginBottom: '30px'}}>
           <ButtonGroup variant="contained" aria-label="Basic button group">
             <Button>One</Button>
             <Button>Two</Button>
             <Button>Three</Button>
           </ButtonGroup>
         </div>
+        <div>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item>
+              <Button variant="contained" color="primary">
+                See my images
+              </Button>
+              <Button variant="outlined" color="secondary">
+                Secondary prop
+              </Button>
+            </Grid>
+          </Grid>
+        </div>
       </main>
+      <Container maxWidth="md" sx={{ marginTop: "30px" }}>
+        <Grid container spacing={4}>
+          {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  image={`https://images.unsplash.com/photo-1729016258424-317d0f05aa28?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
+                  alt={`Image ${card}`}
+                  title={`Image ${card}`}
+                  sx={{
+                    height: 200,
+                    width: "100%",
+                    objectFit: "cover",
+                  }}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">
+                    Card {card}
+                  </Typography>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <footer sx={{ backgroundColor: "lightgreen", padding: "50px 0px" }}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="h6" align="center" gutterBottom>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores,
+          unde!
+        </Typography>
+      </footer>
     </>
   );
 }
